229e495d9ef57e75eba6bec6873f6816b18acb3d
Jean Zapata <jeanza94@hotmail.com>
2023-09-12 00:00:28 -0500

    crud tareas creado

--- apps/tarea/models.py
+++ apps/tarea/models.py
@@ -1,3 +1,4 @@
+from datetime import date
 from django.db import models
 from apps.persona.models import Persona
 
@@ -6,6 +7,7 @@ class Tarea(models.Model):
     title = models.CharField('Titulo', max_length=100, unique=True)
     description = models.TextField('Descripción', max_length=100)
     person = models.ForeignKey(Persona, on_delete=models.CASCADE, related_name='tasks')
+    limit_date = models.DateField('Fecha limite', default=date.today)
 
     class Meta:
         verbose_name = 'Tarea'
--- apps/tarea/views.py
+++ apps/tarea/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Tarea
+from .serializers import TareaSerializer
 
 # Create your views here.
+class TareaView(viewsets.ModelViewSet):
+    serializer_class = TareaSerializer
+    queryset = Tarea.objects.all()
