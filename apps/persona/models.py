7751b63164f91abce19907dfe0f591c2d8039a1e
Jean Zapata <jeanza94@hotmail.com>
2023-09-11 22:48:40 -0500

    creacion de modelo tareas

--- apps/persona/models.py
+++ apps/persona/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class Persona(models.Model):
+    firstname = models.CharField('Primer nombre', max_length=50)
+    middle_name = models.CharField('Segundo nombre', max_length=50)
+    surname = models.CharField('Primer apellido', max_length=50)
+    lastname = models.CharField('Segundo apellido', max_length=50)
+    birth = models.DateField('Fecha de nacimiento')
+    country = models.CharField('País de nacimiento', max_length=50)
+    gender = models.CharField('Genero', max_length=50)
+    status = models.CharField('Estado civíl', max_length=50)
+
+    class Meta:
+        verbose_name = 'Persona'
+        verbose_name_plural = 'Personas'
+    
+    def __str__(self):
+        return f'{self.id} - {self.firstname} - {self.surname}'
+    
+    
+    
--- apps/tarea/models.py
+++ apps/tarea/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from apps.persona.models import Persona
 
 # Create your models here.
+class Tarea(models.Model):
+    title = models.CharField('Titulo', max_length=100, unique=True)
+    description = models.TextField('Descripción', max_length=100)
+    person = models.ForeignKey(Persona, on_delete=models.CASCADE, related_name='tasks')
+
+    class Meta:
+        verbose_name = 'Tarea'
+        verbose_name_plural = 'Tareas'
+
+    def __str__(self):
+        return f'{self.id} - {self.title}'
